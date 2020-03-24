fc17ab9c1be9c89892c8d2a74c6c95cd6b098cbc
Ruma Das <rumaadl16@gmail.com>
2020-03-24 23:33:28 +1030

    Adding hour array in js file

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,9 @@ function getTimeInfo(){
   var formatted = moment(time, "HH:mm:ss").format("LT");
   dateTiming.text(formatted);
 }
-
+// declearing an hour array from 12am to 11pm
+let hourArr = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
+let dateKey;
 
 
 
