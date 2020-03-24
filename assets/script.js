7feed5b7508940a733613550bed10e8a56b3c986
Ruma Das <rumaadl16@gmail.com>
2020-03-25 10:20:49 +1030

    Adding js file

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,12 @@ const headerDate = $("#current-date");
 const container = $(".container");
 let currentHour;
 let currentMinute;
-
-let hourArr = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
+getDateInfo();
+//declearing hour array
+let hourArry = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
 //schedule data is an object with 2 elements, a date and an array of time:event pairs
 let dateKey;
-let todaysSchedule = {
+let todayTime = {
     date : "",
     00 : "",
     01 : "",
@@ -35,14 +36,14 @@ let todaysSchedule = {
     24 : ""
 };
 //start the MomentJS time request before document is fully loaded
-function getDateTimeInfo(){
+function getDateInfo(){
     let date = moment().format('dddd, MMM Do');
     headerDate.text(date);
     currentHour = moment().format('H');
     currentMinute = parseInt(moment().format('mm'));
     dateKey = moment().format('YYYYMMDD');
 }
-getDateTimeInfo();
+
 // get times am pm with moment js from moment
 const time= moment().format('HH:mm:ss');
 var formatted = moment(time, "HH:mm:ss").format("LT");
@@ -50,3 +51,14 @@ let $dateTiming = $("#current-time")
 $dateTiming.text(formatted);
 
 
+function getDayEvents(date){
+  let dateCheck = localStorage.getItem(dateKey);
+  if (dateCheck) {
+    todayTime = JSON.parse(dateCheck);
+  }
+
+  function getHourEvent(hour){
+    return(todayTime[hour]);
+
+}
+}
