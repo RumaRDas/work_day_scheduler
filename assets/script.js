c3e94a3fc7ea08a6eb8db9b47436191d13844ac8
Ruma Das <rumaadl16@gmail.com>
2020-03-25 11:35:17 +1030

    adding code in js file

--- assets/script.js
+++ assets/script.js
@@ -55,52 +55,147 @@ function getDayEvents(date){
   if (dateCheck) {
     todayTime = JSON.parse(dateCheck);
   }
-
+}
   function getHourEvent(hour){
     return(todayTime[hour]);
 }
 //This will be called on page load and will populate any saved info.
 $(document).ready(function() { 
    //function to populate the page with time blocks for 5AM to 12AM
-  function loadTime(){
-   for (let i = 7; i < 18; i++) {
-      let nextRow = $("<div>");
-      let nextColumn;
-      nextRow.addClass("row");
-      for (let k = 0; k < 3; k++){
-        switch (k){               
-          case 0:
-              nextColumn = $("<div>");
-              nextColumn.addClass("col-1 hour");
-              nextColumn.text(hourArry[i]);
-              break;
-              case 1:
-                nextColumn = $("<textarea>");
-                nextColumn.text(getHourEvent(i));
-                if (i < currentHour){
-                    nextColumn.addClass("col-10 details past");
+    function loadSchedule(){
+        //can update this in the future to have the start index be a variable for user to dictate the start of the work day
+        for (let i = 7; i < 18; i++) {
+            let nextRow = $("<div>");
+            let nextColumn;
+            nextRow.addClass("row");
+
+            for (let k = 0; k < 3; k++){
+
+                //check if div or input column
+                switch (k){               
+                    case 0:
+                        nextColumn = $("<div>");
+                        nextColumn.addClass("col-1 hour");
+                        nextColumn.text(hourArry[i]);
+                        break;
+                    //this is the description column which will take input and have dynamic style depending on current hour
+                    case 1:
+                        nextColumn = $("<textarea>");
+                        nextColumn.text(getHourEvent(i));
+                        if (i < currentHour){
+                            nextColumn.addClass("col-10 description past");
+                        }
+                        else if (i == currentHour){
+                            nextColumn.addClass("col-10 description present");
+                        }
+                        else{
+                            nextColumn.addClass("col-10 description future");
+                        }
+                        break;
+                    case 2:
+                        nextColumn = $("<div>");
+                        nextColumn.addClass("col-1 saveBtn text-center");
+                        nextColumn.attr("name", hourArry[i]);
+                        nextColumn.html("<i class='far fa-save mt-4'>");
+                        break;
                 }
-                else if (i == currentHour){
-                    nextColumn.addClass("col-10  details present");
-                }
-                else{
-                    nextColumn.addClass("col-10  details future");
-                }
+                nextRow.append(nextColumn);
+            }
+            container.append(nextRow);        
+        }
+    }
+
+    function saveLocal() {
+        localStorage.setItem(dateKey, JSON.stringify(todayTime));
+    }
+
+    //function to convert 12-hour time to 24 hour numeric values
+    function convertTo24(time){
+        switch (time){
+            case "12AM":
+                return 00;
                 break;
-            case 2:
-                nextColumn = $("<div>");
-                nextColumn.addClass("col-1 saveBtn text-center");
-                nextColumn.attr("name", hourArry[i]);
-                nextColumn.html("<i class='far fa-save mt-4'>");
+            case "1AM":
+                return 01;
+                break;
+            case "2AM":
+                return 02;
+                break;
+            case "3AM":
+                return 03;
+                break;
+            case "4AM":
+                return 04;
+                break;
+            case "5AM":
+                return 05;
+                break;
+            case "6AM":
+                return 06;
+                break;
+            case "7AM":
+                return 07;
+                break;
+            case "8AM":
+                return 08;
+                break;
+            case "9AM":
+                return 09;
+                break;
+            case "10AM":
+                return 10;
+                break;
+            case "11AM":
+                return 11;
+                break;
+            case "12PM":
+                return 12;
+                break;
+            case "1PM":
+                return 13;
+                break;
+            case "2PM":
+                return 14;
+                break;
+            case "3PM":
+                return 15;
+                break;
+            case "4PM":
+                return 16;
+                break;
+            case "5PM":
+                return 17;
+                break;
+            case "6PM":
+                return 18;
+                break;
+            case "7PM":
+                return 19;
+                break;
+            case "8PM":
+                return 20;
+                break;
+            case "9PM":
+                return 21;
+                break;
+            case "10PM":
+                return 22;
+                break;
+            case "11PM":
+                return 23;
                 break;
         }
-        nextRow.append(nextColumn);
-            }
-            container.append(nextRow);      
-           }
-          }
-      
-          getDayEvents(dateKey);
-          loadTime();
-});
-}
+    }
+
+    getDayEvents(dateKey);
+    loadSchedule();
+    
+    //save new schedule text to the session data, then push to local storage
+    $(".saveBtn").on("click", function(event){
+        let eventUpdate = event.currentTarget.previousElementSibling.value;
+        let timeBlock = convertTo24(event.currentTarget.attributes.name.value);
+        todayTime [timeBlock] = eventUpdate;
+        saveLocal();
+    })
+    },);
+
